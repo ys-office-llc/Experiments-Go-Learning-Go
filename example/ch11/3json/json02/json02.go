36c72840c3a0c70ac75427cf7e949e2a4cabf414
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2023-01-04 23:22:29 +0900

    自動整形

--- example/ch11/3json/json02/json02.go
+++ example/ch11/3json/json02/json02.go
@@ -2,26 +2,24 @@ package main
 
 import (
 	"encoding/json"
-	"time"
-	"os"
 	"fmt"
 	"log"
+	"os"
+	"time"
 )
 
-
 type Order struct { // 注文  //liststart1
-	ID            string    `json:"id"`  //注文ID
-	DateOrdered   time.Time `json:"date_ordered"` //注文日時
-	CustomerID    string    `json:"customer_id"` //顧客ID
-	Items         []Item    `json:"items"` //商品
+	ID          string    `json:"id"`           //注文ID
+	DateOrdered time.Time `json:"date_ordered"` //注文日時
+	CustomerID  string    `json:"customer_id"`  //顧客ID
+	Items       []Item    `json:"items"`        //商品
 	//フィールド名は大文字で始める！ jsonのパッケージから見えない
 }
 
 type Item struct {
-	ID   string `json:"id"`  //商品ID
+	ID   string `json:"id"`   //商品ID
 	Name string `json:"name"` //商品名
 } //listend1
-
 
 func main() {
 	data := readData()

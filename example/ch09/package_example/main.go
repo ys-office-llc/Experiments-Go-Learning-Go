767ddc10e6aa70dddad7255d32b5d08c8f805b30
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2023-01-04 11:13:44 +0900

    自動整形したら print というパッケージ名が掘り起こされた

--- example/ch09/package_example/main.go
+++ example/ch09/package_example/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"example.co.jp/package_example/formatter" // パッケージprint
-	"example.co.jp/package_example/math" // パッケージmath
+
+	print "example.co.jp/package_example/formatter" // パッケージprint
+	"example.co.jp/package_example/math"            // パッケージmath
 )
 
 func main() {
-	num := math.Double(2)  // パッケージmath（math/math.go）
+	num := math.Double(2)       // パッケージmath（math/math.go）
 	output := print.Format(num) // パッケージprint（formatter/formatter.go）
-	fmt.Println(output) 
+	fmt.Println(output)
 }

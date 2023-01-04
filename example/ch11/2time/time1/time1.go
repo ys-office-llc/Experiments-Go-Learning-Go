cfe7f417f3d20c242c13ffd991f2e33ec54370dc
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2023-01-04 14:44:47 +0900

    戻り値をコメントで追加

--- example/ch11/2time/time1/time1.go
+++ example/ch11/2time/time1/time1.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	//liststart1
-	fmt.Println(time.Hour)
-	fmt.Println(time.Minute)
-	fmt.Println(time.Second)
+	fmt.Println(time.Hour)                             // 1h0m0s
+	fmt.Println(time.Minute)                           // 1m0s
+	fmt.Println(time.Second)                           // 1s
 	d := 2*time.Hour + 30*time.Minute + 45*time.Second // d の型はtime.Duration
 	fmt.Println(d)                                     // 2h30m45s
 	//listend1

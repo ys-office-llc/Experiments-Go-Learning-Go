a3296747203b67662d374ae1abe9a5213afda996
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2023-01-07 17:28:41 +0900

    変数の初期化と、インターフェイスの変数へ入れ替えを省略した版

---
+++ example/interface/eating_omitted/eating.go
@@ -0,0 +1,59 @@
+// https://qiita.com/rtok/items/46eadbf7b0b7a1b0eb08
+package main
+
+import "fmt"
+
+// 食べるためのインターフェース
+type Eater interface {
+	PutIn()   // 口に入れる
+	Chew()    // 噛む
+	Swallow() // 飲み込む
+}
+
+// 人間の構造体
+type Human struct {
+	Height int // 身長
+}
+
+// カメの構造体
+type Turtle struct {
+	Kind string // 種類
+}
+
+// 人間用のインターフェースの実装
+func (h Human) PutIn() {
+	fmt.Println("道具を使って丁寧に口に運ぶ")
+}
+func (h Human) Chew() {
+	fmt.Println("歯でしっかり噛む")
+}
+func (h Human) Swallow() {
+	fmt.Println("よく噛んだら飲み込む")
+}
+
+// カメ用のインターフェースの実装
+func (h Turtle) PutIn() {
+	fmt.Println("獲物を見つけたら首をすばやく伸ばして噛む")
+}
+func (h Turtle) Chew() {
+	fmt.Println("クチバシで噛み砕く")
+}
+func (h Turtle) Swallow() {
+	fmt.Println("小さく砕いたら飲み込む")
+}
+
+// インターフェースが引数になる、食べるメソッド
+func EatAll(e Eater) {
+	e.PutIn() // インターフェースから呼び出す
+	e.Chew()
+	e.Swallow()
+}
+
+func main() {
+	man := Human{Height: 300}               // 人間用の構造体を作成
+	cheloniaMydas := Turtle{Kind: "アオウミガメ"} // カメ用の構造体を作成
+	fmt.Println("＜人間が食べる＞")
+	EatAll(man) // インターフェースを引数に関数を呼ぶ
+	fmt.Println("＜カメが食べる＞")
+	EatAll(cheloniaMydas)
+}

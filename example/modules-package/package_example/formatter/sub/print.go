2c7ac71f8c2eda69848ae8f09f037109ca790273
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2023-01-08 20:34:50 +0900

    パッケージ構造再構成（非推奨パターン排除）⇒　https://zenn.dev/_kazuya/articles/ce19973159a028

--- example/modules-package/package_example/formatter/sub/print.go
+++
@@ -1,7 +0,0 @@
-package print_sub
-
-import "fmt"
-
-func PrintSub(num int) string {
-	return fmt.Sprintf("数字は%dです。", num)
-}
--- example/modules-package/package_example/main.go
+++ example/modules-package/package_example/main.go
@@ -3,20 +3,15 @@ package main
 import (
 	"fmt"
 
-	"example.co.jp/package_example/aws/s3"
-	print "example.co.jp/package_example/formatter"         // パッケージprint
-	print_sub "example.co.jp/package_example/formatter/sub" // パッケージ print_sub
-	"example.co.jp/package_example/math"                    // パッケージmath
+	"example.co.jp/package_example/aws/s3"          // パッケージ s3
+	print "example.co.jp/package_example/formatter" // パッケージprint
+	"example.co.jp/package_example/math"            // パッケージmath
 )
 
 func main() {
 	num := math.Double(2)       // パッケージmath（math/math.go）
 	output := print.Format(num) // パッケージprint（formatter/formatter.go）
-	output2 := print.Print(num * 2)
-	output3 := print_sub.PrintSub(num * 3)
-	o := s3.PutObject("image.png")
+	r := s3.PutObject("image.png")
 	fmt.Println(output)
-	fmt.Println(output2)
-	fmt.Println(output3)
-	fmt.Println(o)
+	fmt.Println(r)
 }
